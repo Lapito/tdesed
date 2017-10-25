d91c9f8634c46efab8d3acf1bc659852382543b0
Lapito <matheuslemos21@gmail.com>
2017-10-25 21:50:46 -0200

    Update huff.c
    Gera árvore de huffman e imprime ela em pre-order

--- huff.c
+++ huff.c
@@ -12,13 +12,19 @@ struct sNo {
 	struct sNo *dir;
 }typedef sNo;
 
+struct tSim {
+	int val;
+	char cod[256];
+}typedef tSim;
+
 sNo *inicializa();
 sNo *cria(int valor, int frequencia, sNo *sae, sNo *sad);
 void compactar(char *arquivo);
 void ordena(sNo *fila, int tam);
-sNo *tiraPrimeiro(sNo *fila, int tamanho);
+sNo *tiraUltimo(sNo *fila, int tamanho);
 int tamanhoFila(sNo *fila);
-sNo *criaHuffman(sNo *no1, sNo *no2);
+//sNo *criaHuffman(sNo *no1, sNo *no2);
+sNo *criaHuffman(sNo *fila, int tamanho);
 void imprime_textual(sNo *a) {
 	if (a == NULL)
 		printf("<>");
@@ -29,6 +35,16 @@ void imprime_textual(sNo *a) {
 		printf(">");
 	}
 }
+
+void criaTsim(tSim *tsim) { //Tabela de simbolos
+	tSim *aux;
+	static char trail[256];
+
+
+
+}
+
+
 //FAZER FUNCAO PARA PEGAR O TAMANHO DO VETOR
 
 
@@ -76,6 +92,7 @@ void compactar(char *arquivo) {
 	//sNo *fila;
 
 	static sNo fila[256];
+	tSim tsim[256];
 
 	//fila[256];
 
@@ -131,23 +148,13 @@ void compactar(char *arquivo) {
 			printf("%d->%c\n", fila[i].freq,fila[i].val);
 		}
 		
-		sNo *hufftree, *notemp1, *notemp2;
-		hufftree = (sNo*)calloc(1, sizeof(sNo));
-		notemp1 = (sNo*)calloc(1, sizeof(sNo));
-		notemp2 = (sNo*)calloc(1, sizeof(sNo));
+		sNo hufftree;
 
-		while (tam > 1) {
-			notemp1 = tiraPrimeiro(&*fila, tam);
-			notemp2 = tiraPrimeiro(&*fila, tamanhoFila(&*fila));
-			hufftree = criaHuffman(&*notemp1, &*notemp2);
-			printf("%d %d %d", hufftree->freq, hufftree->esq->freq, hufftree->dir->freq);
-			tam = tamanhoFila(&*fila);
-			fila[tam] = *hufftree;
-			ordena(&*fila, tamanhoFila(&*fila));
-			tam = tamanhoFila(&*fila);
-		}
+		printf("\n\n%x\n\n", &fila); //imprime local da fila so pq nn sei queria ver no q dava
+
 		printf("\n\n");
-		imprime_textual(hufftree);
+		hufftree = *criaHuffman(&fila, tam);
+		imprime_textual(&hufftree);
 
 
 	}
@@ -191,14 +198,12 @@ void ordena(sNo *fila, int tam) {
 	free(aux);
 }
 
-sNo *tiraPrimeiro(sNo *fila, int tamanho) {
+sNo *tiraUltimo(sNo *fila, int tamanho) {
 	sNo *aux;
 	int i;
 	aux = cria(0, 0, inicializa(), inicializa());
-	*aux = fila[0];
-	for (i = 0; i < tamanho; i++) {
-		fila[i] = fila[i + 1];
-	}
+	*aux = fila[tamanho - 1];
+	fila[tamanho - 1] = *cria(0, 0, inicializa(), inicializa());
 	return aux;
 }
 
@@ -209,10 +214,35 @@ int tamanhoFila(sNo *fila) {
 	}
 	return count;
 }
-
+/*
 sNo *criaHuffman(sNo *no1, sNo *no2) {
 	sNo *notemp;
 	notemp = (sNo *)calloc(1, sizeof(sNo));
 	notemp = cria(0, (no1->freq) + (no2->freq), no1, no2);
 	return notemp;
+}*/
+
+sNo *criaHuffman(sNo *fila, int tamanho) {
+	int tam = tamanho;
+	sNo *hufftree, *notemp1, *notemp2;
+	hufftree = (sNo*)calloc(1, sizeof(sNo));
+	notemp1 = (sNo*)calloc(1, sizeof(sNo));
+	notemp2 = (sNo*)calloc(1, sizeof(sNo));
+
+	while (tam > 1) {
+		notemp1 = tiraUltimo(&*fila, tam);
+		tam--;
+		notemp2 = tiraUltimo(&*fila, tam);
+		tam--;
+		hufftree = cria(0, (notemp1->freq) + (notemp2->freq), notemp1, notemp2);
+		fila[tam] = *hufftree;
+		tam++;
+		ordena(&*fila, tam);
+	}
+
+	//free(notemp1);
+	//free(notemp2);
+	
+	return hufftree;
+
 }
